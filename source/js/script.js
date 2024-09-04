92a35e80bbbd77fb1df1819c2156ab0975cf8c3d
weitsung50110 <a1233z51ab10@gmail.com>
2024-09-04 14:16:36 +0800

    using url_for function, do not need root any more

--- source/js/script.js
+++ source/js/script.js
@@ -1,37 +1,37 @@
+
 document.addEventListener('DOMContentLoaded', function () {
-  // 初始隐藏下拉菜单和社交链接侧边栏
+  // 取得下拉菜單和切換按鈕
   const navMenu = document.querySelector('.nav-menu');
+  const menuButton = document.querySelector('.menu-button');
+  const toggleButton = document.querySelector('#toggleButton');
   const socialSidebar = document.querySelector('.social-sidebar');
 
-  navMenu.style.opacity = '0'; // 初始透明度为0，隐藏菜单
   socialSidebar.style.transform = 'translateX(100%)'; // 初始隐藏社交链接侧边栏
 
-  // 滚动到特定部分
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
-    });
-  });
+  // 初始化下拉菜單狀態
+  navMenu.style.opacity = '0';
+  navMenu.style.pointerEvents = 'none';
 
-  // 下拉菜单
-  const menuButton = document.querySelector('.menu-button');
-
+  // 點擊下拉菜單按鈕顯示/隱藏菜單
   menuButton.addEventListener('click', () => {
     if (navMenu.style.opacity === '0') {
       navMenu.style.opacity = '1';
-      navMenu.style.pointerEvents = 'auto'; // 显示时启用点击事件
+      navMenu.style.pointerEvents = 'auto'; // 顯示時啟用點擊事件
     } else {
       navMenu.style.opacity = '0';
-      navMenu.style.pointerEvents = 'none'; // 隐藏时禁用点击事件
+      navMenu.style.pointerEvents = 'none'; // 隱藏時禁用點擊事件
     }
   });
 
-  // 社交链接侧边栏
-  const toggleButton = document.querySelector('#toggleButton');
+  // 點擊其他地方隱藏下拉菜單
+  document.addEventListener('click', (event) => {
+    if (!menuButton.contains(event.target) && !navMenu.contains(event.target)) {
+      navMenu.style.opacity = '0';
+      navMenu.style.pointerEvents = 'none';
+    }
+  });
 
+  // 點擊 toggle 按鈕的行為
   toggleButton.addEventListener('click', () => {
     if (socialSidebar.style.transform === 'translateX(100%)') {
       socialSidebar.style.transform = 'translateX(0)';
@@ -39,5 +39,4 @@ document.addEventListener('DOMContentLoaded', function () {
       socialSidebar.style.transform = 'translateX(100%)';
     }
   });
-
 });
