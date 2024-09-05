04b7a4aa73d3dc507a8e000676f514633713f10a
weitsung50110 <a1233z51ab10@gmail.com>
2024-09-05 13:36:26 +0800

    modify side bar to the left side

--- source/js/side_bar_script.js
+++ source/js/side_bar_script.js
@@ -1,26 +1,25 @@
-
 document.addEventListener('DOMContentLoaded', function () {
   // 取得切換按鈕
   const toggleButton = document.querySelector('#toggleButton');
   const toggleButton2 = document.querySelector('#toggleButton2');
   const socialSidebar = document.querySelector('.social-sidebar');
 
-  socialSidebar.style.transform = 'translateX(100%)'; // 初始隐藏社交链接侧边栏
+  socialSidebar.style.transform = 'translateX(-100%)'; // 初始隐藏社交链接侧边栏
   
   // 點擊 toggle 按鈕的行為
   toggleButton.addEventListener('click', () => {
-    if (socialSidebar.style.transform === 'translateX(100%)') {
+    if (socialSidebar.style.transform === 'translateX(-100%)') {
       socialSidebar.style.transform = 'translateX(0)';
     } else {
-      socialSidebar.style.transform = 'translateX(100%)';
+      socialSidebar.style.transform = 'translateX(-100%)';
     }
   });
 
   toggleButton2.addEventListener('click', () => {
-    if (socialSidebar.style.transform === 'translateX(100%)') {
+    if (socialSidebar.style.transform === 'translateX(-100%)') {
       socialSidebar.style.transform = 'translateX(0)';
     } else {
-      socialSidebar.style.transform = 'translateX(100%)';
+      socialSidebar.style.transform = 'translateX(-100%)';
     }
   });
 });
